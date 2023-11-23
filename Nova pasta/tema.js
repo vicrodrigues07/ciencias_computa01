4de1dc38f04f765efc5567ac237f0a68dac37721
vicrodrigues07 <142310764+vicrodrigues07@users.noreply.github.com>
2023-11-23 12:21:08 -0300

    Add files via upload

---
+++ Nova pasta/tema.js
@@ -0,0 +1,12 @@
+
+var botao = document.querySelector ('#Tema');
+
+
+botao.onclick = function () {
+   document.body.classList.toggle ('#Tema_claro');
+   document.body.classList.toggle ('#tema_escuro');
+};
+
+
+
+
